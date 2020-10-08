9c46f4b56b89f66fbd266203869aa14f356ed846
lsilva92 <leandro.felipesilva@hotmail.com>
2020-10-08 22:56:44 +0100

    Improved stripe

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const compression = require('compression');
 const cors = require('cors');
 
@@ -54,9 +55,17 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
+/*
 app.post(
   '/webhook-checkout',
   express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);*/
+
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
+app.post(
+  '/webhook-checkout',
+  bodyParser.raw({ type: 'application/json' }),
   bookingController.webhookCheckout
 );
 
