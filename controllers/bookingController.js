6251b0bdef78f8308bf885074114a675cee5a412
lsilva92 <leandro.felipesilva@hotmail.com>
2020-10-08 22:49:59 +0100

    Improved stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,21 +1,22 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const Booking = require('./../models/bookingModel');
-const Tour = require('./../models/tourModel');
+const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-  //1) Get the currently booked tour
+  // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
+  // console.log(tour);
 
-  //2) Create getCheckout Session
+  // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    /* success_url: `${req.protocol}://${req.get('host')}/?tour=${
-      req.params.tourId
-    }&user=${req.user.id}&price=${tour.price}`,*/
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+    //   req.params.tourId
+    // }&user=${req.user.id}&price=${tour.price}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -32,30 +33,18 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       }
     ]
   });
-  //3) Create session as response
+
+  // 3) Create session as response
   res.status(200).json({
     status: 'success',
     session
   });
 });
 
-/*exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-  //this is only TEMPORARY, because it's UNSECURE: everyone can book tour without paying
-  const { tour, user, price } = req.query;
-
-  if (!tour && !user && !price) return next();
-  await Booking.create({ tour, user, price });
-
-  res.redirect(req.originalUrl.split('?')[0]);
-});*/
-
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.display_items[0].amount / 100;
-
-  console.log(user);
-
   await Booking.create({ tour, user, price });
 };
 
@@ -80,7 +69,7 @@ exports.webhookCheckout = (req, res, next) => {
 };
 
 exports.createBooking = factory.createOne(Booking);
-exports.getAllBookings = factory.getAll(Booking);
 exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
 exports.updateBooking = factory.updateOne(Booking);
 exports.deleteBooking = factory.deleteOne(Booking);
